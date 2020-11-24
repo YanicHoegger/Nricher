6b267802a8325f7c9c90cb37452fab5527c7129a
Yanic Högger <yanic.hoegger@gmx.ch>
2020-11-24 10:43:17 +0100

    - Added extension methods to add service without decorator

---
+++ source/DependencyInjectionExtensions/Decorator/AddWithoutDecoratorServiceCollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjectionExtensions.Decorator
+{
+    public static class AddWithoutDecoratorServiceCollectionExtensions
+    {
+        public static void AddSingletonWithoutDecorator<TService, TImplementation>(this IServiceCollection serviceCollection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddSingleton<TService, TImplementation>());
+        }
+
+        public static void AddSingletonWithoutDecorator<TService, TImplementation>(this IServiceCollection serviceCollection,
+            Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddSingleton<TService, TImplementation>(implementationFactory));
+        }
+
+        public static void AddSingletonWithoutDecorator<TService>(this IServiceCollection serviceCollection,
+            TService instance)
+            where TService : class
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddSingleton(instance));
+        }
+
+        public static void AddScopedWithoutDecorator<TService, TImplementation>(this IServiceCollection serviceCollection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddScoped<TService, TImplementation>());
+        }
+
+        public static void AddScopedWithoutDecorator<TService, TImplementation>(this IServiceCollection serviceCollection,
+            Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddScoped<TService, TImplementation>(implementationFactory));
+        }
+
+        public static void AddTransientWithoutDecorator<TService, TImplementation>(
+            this IServiceCollection serviceCollection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddTransient<TService, TImplementation>());
+        }
+
+        public static void AddTransientWithoutDecorator<TService, TImplementation>(this IServiceCollection serviceCollection,
+            Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddWithoutExtension<DecoratorExtension>(x => x.AddTransient<TService, TImplementation>(implementationFactory));
+        }
+    }
+}
--- source/DependencyInjectionExtensions/SimpleServiceCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace DependencyInjectionExtensions
-{
-    public class SimpleServiceCollection : List<ServiceDescriptor>, IServiceCollection
-    {
-    }
-}
