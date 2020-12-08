87d076fa20f4e32893c309746d38fe48d3006eec
Yanic Högger <yanic.hoegger@gmx.ch>
2020-12-08 18:40:19 +0100

    Refactoring

--- source/DependencyInjectionExtensions/Decorator/DecoratorExtension.cs
+++ source/DependencyInjectionExtensions/Decorator/DecoratorExtension.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DependencyInjectionExtensions.Decorator
 {
     public class DecoratorExtension : IServiceCollectionExtension
     {
+        private readonly ImplementationFactoryFactory _factory;
+
         public DecoratorExtension(IDecoratorFactory decoratorFactory)
         {
             DecoratorFactory = decoratorFactory;
+            _factory = new ImplementationFactoryFactory(decoratorFactory);
         }
 
         public IDecoratorFactory DecoratorFactory { get; }
@@ -29,19 +30,16 @@ namespace DependencyInjectionExtensions.Decorator
             //We need the already existent entry so we can decorate any number of times 
             var alreadyRegisterDescriptor = serviceCollection.Last(x => x.ServiceType == serviceDescriptor.ServiceType);
 
-            ServiceDescriptor newServiceDescriptor;
+            Func<IServiceProvider, object> implementationFactory;
             if (alreadyRegisterDescriptor.ImplementationInstance != null)
             {
-                newServiceDescriptor = new ServiceDescriptor(alreadyRegisterDescriptor.ServiceType,
-                    provider => CreateDecorated(alreadyRegisterDescriptor.ImplementationInstance,
-                        alreadyRegisterDescriptor.ServiceType, provider),
-                    alreadyRegisterDescriptor.Lifetime);
+                implementationFactory = _factory.CreateFromInstance(alreadyRegisterDescriptor.ImplementationInstance,
+                    alreadyRegisterDescriptor.ServiceType);
             }
             else if (alreadyRegisterDescriptor.ImplementationFactory != null)
             {
-                newServiceDescriptor = new ServiceDescriptor(alreadyRegisterDescriptor.ServiceType,
-                    CreateFromImplementationFactory(alreadyRegisterDescriptor.ImplementationFactory, serviceDescriptor.ServiceType),
-                    alreadyRegisterDescriptor.Lifetime);
+                implementationFactory = _factory.CreateFromImplementationFactory(alreadyRegisterDescriptor.ImplementationFactory,
+                    serviceDescriptor.ServiceType);
             }
             else
             {
@@ -61,76 +59,14 @@ namespace DependencyInjectionExtensions.Decorator
                     serviceCollection.Add(new ServiceDescriptor(implementationType, implementationType, alreadyRegisterDescriptor.Lifetime));
                 }
 
-                newServiceDescriptor = new ServiceDescriptor(alreadyRegisterDescriptor.ServiceType,
-                    CreateFromType(implementationType, alreadyRegisterDescriptor.ServiceType),
-                    alreadyRegisterDescriptor.Lifetime);
+                implementationFactory = _factory.CreateFromType(implementationType, alreadyRegisterDescriptor.ServiceType);
             }
+
+            var newServiceDescriptor = new ServiceDescriptor(alreadyRegisterDescriptor.ServiceType,
+                implementationFactory, 
+                alreadyRegisterDescriptor.Lifetime);
 
             ServiceCollectionHelper.ReplaceServiceDescriptor(newServiceDescriptor, serviceCollection);
-        }
-
-        /// <param name="implementationFactory"></param>
-        /// <param name="serviceType"></param>
-        /// <returns>serviceProvider =&gt; <see cref="DecoratorFactory"/>.<see cref="IDecoratorFactory.CreateDecorated"/>(<paramref name="implementationFactory"/>(), <paramref name="serviceType"/>, serviceProvider)</returns>
-        private Func<IServiceProvider, object> CreateFromImplementationFactory(Func<IServiceProvider, object> implementationFactory, Type serviceType)
-        {
-            var inputParameter = Expression.Parameter(typeof(IServiceProvider), "serviceProvider");
-
-            Expression<Func<IServiceProvider, object>> implementationFactoryExpression = provider => implementationFactory(provider);
-            var invokedImplementationFactory = Expression.Invoke(implementationFactoryExpression, inputParameter);
-
-            var createDecoratorMethod = typeof(DecoratorExtension).GetMethod(nameof(CreateDecorated), BindingFlags.NonPublic | BindingFlags.Instance);
-            var callCreateDecorator = Expression.Call(Expression.Constant(this),
-                createDecoratorMethod!,
-                invokedImplementationFactory,
-                Expression.Constant(serviceType),
-                inputParameter);
-
-            var lambdaExpression = Expression.Lambda(callCreateDecorator, inputParameter);
-
-            return (Func<IServiceProvider, object>)lambdaExpression.Compile();
-        }
-
-        /// <param name="implementationType"></param>
-        /// <param name="serviceType"></param>
-        /// <returns>serviceProvider =&gt; <see cref="DecoratorFactory"/>.<see cref="IDecoratorFactory.CreateDecorated"/>(serviceProvider.GetService(<paramref name="implementationType"/>), <paramref name="serviceType"/>, serviceProvider)</returns>
-        private Func<IServiceProvider, object> CreateFromType(Type implementationType, Type serviceType)
-        {
-            var inputParameter = Expression.Parameter(typeof(IServiceProvider), "serviceProvider");
-
-            var getServiceMethod = typeof(IServiceProvider).GetMethod(nameof(IServiceProvider.GetService));
-            Debug.Assert(getServiceMethod != null, nameof(getServiceMethod) + " != null");
-            var callImplementation = Expression.Call(inputParameter, getServiceMethod!, Expression.Constant(implementationType));
-
-            var createDecoratorMethod = typeof(DecoratorExtension).GetMethod(nameof(CreateDecorated), BindingFlags.NonPublic | BindingFlags.Instance);
-            var callCreateDecorator = Expression.Call(Expression.Constant(this),
-                createDecoratorMethod!,
-                callImplementation,
-                Expression.Constant(serviceType),
-                inputParameter);
-
-            var lambdaExpression = Expression.Lambda(callCreateDecorator, inputParameter);
-
-            return (Func<IServiceProvider, object>)lambdaExpression.Compile();
-        }
-
-        private object CreateDecorated(object toDecorate, Type decoratingType, IServiceProvider serviceProvider)
-        {
-            var decorated = DecoratorFactory.CreateDecorated(toDecorate, decoratingType, serviceProvider);
-
-            var interfaces = toDecorate.GetType().GetInterfaces();
-            var areImplemented = decorated.GetType().GetInterfaces().ToList();
-            if (interfaces.All(x => areImplemented.Contains(x)))
-            {
-                return decorated;
-            }
-
-            var dynamicType = DynamicTypeCreator.CreateDynamicInterface(interfaces, toDecorate.GetType().Name);
-
-            var methodInfo = typeof(InterfaceEnsurerDecorator).GetMethod(nameof(InterfaceEnsurerDecorator.Create));
-            Debug.Assert(methodInfo != null, nameof(methodInfo) + " != null");
-
-            return methodInfo.MakeGenericMethod(dynamicType).Invoke(null, new[] { toDecorate, decorated })!;
         }
     }
 }
---
+++ source/DependencyInjectionExtensions/Decorator/DecoratorImplementationFactoryFactory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace DependencyInjectionExtensions.Decorator
+{
+    public class DecoratorImplementationFactoryFactory
+    {
+        private readonly IDecoratorFactory _decoratorFactory;
+
+        public DecoratorImplementationFactoryFactory(IDecoratorFactory decoratorFactory)
+        {
+            _decoratorFactory = decoratorFactory;
+        }
+
+        public Func<IServiceProvider, object> CreateFromInstance(object instance, Type serviceType)
+        {
+            return provider => CreateDecorated(instance, serviceType, provider);
+        }
+
+        public Func<IServiceProvider, object> CreateFromImplementationFactory(Func<IServiceProvider, object> implementationFactory, Type serviceType)
+        {
+            var inputParameter = Expression.Parameter(typeof(IServiceProvider), "serviceProvider");
+
+            Expression<Func<IServiceProvider, object>> implementationFactoryExpression = provider => implementationFactory(provider);
+            var invokedImplementationFactory = Expression.Invoke(implementationFactoryExpression, inputParameter);
+
+            var createDecoratorMethod = typeof(DecoratorImplementationFactoryFactory).GetMethod(nameof(CreateDecorated), BindingFlags.NonPublic | BindingFlags.Instance);
+            var callCreateDecorator = Expression.Call(Expression.Constant(this),
+                createDecoratorMethod!,
+                invokedImplementationFactory,
+                Expression.Constant(serviceType),
+                inputParameter);
+
+            var lambdaExpression = Expression.Lambda(callCreateDecorator, inputParameter);
+
+            return (Func<IServiceProvider, object>)lambdaExpression.Compile();
+        }
+
+        public Func<IServiceProvider, object> CreateFromType(Type implementationType, Type serviceType)
+        {
+            return CreateFromImplementationFactory(provider => provider.GetService(implementationType), serviceType);
+        }
+
+        private object CreateDecorated(object toDecorate, Type decoratingType, IServiceProvider serviceProvider)
+        {
+            var decorated = _decoratorFactory.CreateDecorated(toDecorate, decoratingType, serviceProvider);
+
+            var interfaces = toDecorate.GetType().GetInterfaces();
+            var areImplemented = decorated.GetType().GetInterfaces().ToList();
+            if (interfaces.All(x => areImplemented.Contains(x)))
+            {
+                return decorated;
+            }
+
+            var dynamicType = DynamicTypeCreator.CreateDynamicInterface(interfaces, toDecorate.GetType().Name);
+
+            var methodInfo = typeof(InterfaceEnsurerDecorator).GetMethod(nameof(InterfaceEnsurerDecorator.Create));
+            Debug.Assert(methodInfo != null, nameof(methodInfo) + " != null");
+
+            return methodInfo.MakeGenericMethod(dynamicType).Invoke(null, new[] { toDecorate, decorated })!;
+        }
+    }
+}
